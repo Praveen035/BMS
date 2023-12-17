66553f607c062dd42b8a99c7b905ed565b82d04b
Praveen035 <praveen@antbuild.in>
2023-12-17 23:23:17 +0530

    created dashboard component

--- Client/src/app/components/layout/layout-routing.module.ts
+++ Client/src/app/components/layout/layout-routing.module.ts
@@ -5,7 +5,31 @@ import { LayoutComponent } from './component/layout.component';
 export const LayoutRoutes: Routes = [
   {
     path: '',
-    component: LayoutComponent
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'dashboard',
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./../dashboard/dashboard.module').then(m => m.DashboardModule),
+
+      },
+      {
+        path: 'employee',
+        loadChildren: () => import('../employee/employee.module').then(m => m.EmployeeModule),
+      },
+      {
+        path: 'customer',
+        loadChildren: () => import('../customer/customer.module').then(m => m.CustomerModule),
+      },
+      {
+        path: 'department',
+        loadChildren: () => import('../department/department.module').then(m => m.DepartmentModule),
+      }
+    ]
   }
 ];
 
