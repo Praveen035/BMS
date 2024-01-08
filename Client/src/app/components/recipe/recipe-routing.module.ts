2d6c018f40a29f189570ab26463bc81de1a3709d
Praveen035 <praveen@antbuild.in>
2024-01-08 12:57:37 +0530

    created recipe and recipe category classes and services

--- Client/src/app/components/recipe/recipe-routing.module.ts
+++ Client/src/app/components/recipe/recipe-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { CreateRecipeComponent } from './create-recipe/create-recipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: RecipeDetailsComponent
+},
+{
+  path: 'create-recipe',
+  component: CreateRecipeComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
