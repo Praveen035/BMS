7cae2a0f74d073a347e49b10ed3718c0ae4e0061
Praveen035 <praveenplus054@gmail.com>
2024-05-10 09:48:24 +0530

    Remove ignored files

---
+++ Server/Service/FakeCalculator.cs
@@ -0,0 +1,33 @@
+﻿using IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class FakeCalculator : ICalculator
+    {
+        public decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public decimal Divide(decimal num1, decimal num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual decimal Multiply(decimal num1, decimal num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal Substract(decimal num1, decimal num2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
--- Server/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Server/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3ff0f07f732534d548b78bf0af4ff36bfdbb98e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca360bffebd3bab0274591dff7483ac83ade393")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
